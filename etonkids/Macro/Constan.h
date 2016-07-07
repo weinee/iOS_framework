194de4ec697c2f367e1f007be37bb4309a66eec1
weineeL <LiJF_wn@163.com>
2016-07-07 23:29:57 +0800

    框架

---
+++ etonkids/Macro/Constan.h
@@ -0,0 +1,32 @@
+//
+//  Constan.h
+//  etonkids
+//
+//  Created by weineeL on 16/7/4.
+//  Copyright © 2016年 ytdinfo. All rights reserved.
+//
+
+#ifndef Constan_h
+#define Constan_h
+/** app key*/
+//oneapm 性能监控
+#define appkey_oneapm @"ADACCA9E4AA5AFE7D7C5594BFB6E039076"
+
+//百度地图key
+#define BMKKEY @"QlieoECYSow6LYphIAVVt25DDw71pGz1"
+
+/** 通用*/
+//图片压缩比例
+#define Image_compression 0.3f
+//长文本长度限制
+#define TEXT_LENGTH 125
+
+#define PI 3.1416
+#define PAGE_SIEZ 10
+
+//每米多少维度
+#define LatitudeDeltaPerMeter 9.009009e-6
+//每米多少经度
+#define LongitudeDeltaPerMeter 1.171097e-5
+
+#endif /* Constan_h */
--- etonkids/Macro/Macros.h
+++ etonkids/Macro/Macros.h
@@ -8,13 +8,6 @@
 
 #import <Foundation/Foundation.h>
 
-/** app key*/
-//oneapm 性能监控
-#define appkey_oneapm @"ADACCA9E4AA5AFE7D7C5594BFB6E039076"
-
-//百度地图key
-#define BMKKEY @"QlieoECYSow6LYphIAVVt25DDw71pGz1"
-
 ///Size
 #define kScreenWidth [UIScreen mainScreen].bounds.size.width
 #define kScreenHeight [UIScreen mainScreen].bounds.size.height
@@ -99,17 +92,3 @@
 //占位图片
 #define PLACEHLODER_IMAGE [UIImage imageNamed:@"icon-image-placeholder"]
 #define WORKCIRCEL_DEFAULT_IMAGE [UIImage imageNamed:@"pbg.jpg"]
-
-/** 通用*/
-//图片压缩比例
-#define Image_compression 0.3f
-//长文本长度限制
-#define TEXT_LENGTH 125
-
-#define PI 3.1416
-#define PAGE_SIEZ 10
-
-//每米多少维度
-#define LatitudeDeltaPerMeter 9.009009e-6
-//每米多少经度
-#define LongitudeDeltaPerMeter 1.171097e-5
